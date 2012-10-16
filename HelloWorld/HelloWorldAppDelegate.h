5d5353c65940fe1695938f20118ce3b219202d13
Donna Kocherry <donna@qburst.com>
2012-10-16 11:53:00 +0530

    HelloWorld/

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by user on 10/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by user on 10/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+<UITextFieldDelegate>
+@property (copy, nonatomic) NSString *userName;
+
+
+@end
